eae35110cecb7aada700cc099d7b864a6de06d10
baluds <baluds1999@gmail.com>
2020-04-27 15:09:59 +0530

    Employee table and parking lot table complete

--- main.py
+++ main.py
@@ -60,14 +60,28 @@ def upload_page():
 
             extracted_text = ocr_core(file)
 
+            pk_collection = mongo.db.lot
+
+            pk_up = pk_collection.find_one({'Lot Name' : space_no})
+
             exit = user_collection.find_one({'Vehicle Number' : extracted_text})
 
             if exit is None:
                 user_collection.insert({'Vehicle Number' : extracted_text,'Parking Lot' : space_no,'Date' : dateStr,'In time' : timeStr,'Managed by' : manager,'Out date' : '','Out time' : ''})
+                pk_up["Space left"] = pk_up["Space left"] - 1
+                pk_collection.save(pk_up)
+
+
             else:
                 exit["Out date"] = dateStr
                 exit["Out time"] = timeStr
+                pk_al = exit["Parking Lot"]
                 user_collection.save(exit)
+                pk_al1 = pk_collection.find_one({'Lot Name' : pk_al})
+                pk_al1["Space left"] = pk_al1["Space left"] + 1
+                pk_collection.save(pk_al1)
+
+
 
             all_veh = mongo.db.users.find()
 
@@ -86,3 +100,40 @@ def upload_page():
 @main.route('/emp', methods=['GET', 'POST'])
 def emp_page():
     if request.method == 'POST':
+        eid = request.form['emp_id']
+        ename = request.form['emp_name']
+        eveh = request.form['emp_veh']
+        dateTimeObj2 = datetime.now()
+        dateStr2 = dateTimeObj2.strftime("%d %b %Y ")
+        timeStr2 = dateTimeObj2.strftime("%I:%M %p")
+        emp_collection = mongo.db.employee
+        find1 = emp_collection.find_one({'Employee Name' : ename})
+        if find1 is None:
+            emp_collection.insert({'Employee ID' : eid,'Employee Name' : ename,'Employee Vehicle' : eveh,'In Date' : dateStr2,'In time' : timeStr2,'Out date' : '','Out time' : ''})
+        else:
+            find1["Out date"] = dateStr2
+            find1["Out time"] = timeStr2
+            emp_collection.save(find1)
+
+        emp_details = mongo.db.employee.find()
+        return render_template('emp.html',emp_details=emp_details)
+    elif request.method == 'GET':
+        emp_details = mongo.db.employee.find()
+        return render_template('emp.html',emp_details=emp_details)
+
+
+@main.route('/lot')
+def lot_page():
+    pk_collection = mongo.db.lot
+    pk_details = mongo.db.lot.find()
+    return render_template('parking_space.html',pk_details=pk_details)
+
+
+
+                
+               
+
+        
+        
+
+
