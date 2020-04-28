39c6023495d61f13cd4113036edce7ccf95add64
baluds <baluds1999@gmail.com>
2020-04-29 03:55:53 +0530

    homepage:Completed

--- main.py
+++ main.py
@@ -37,14 +37,20 @@ def home_page():
 def upload_page():
     if request.method == 'POST':
         # check if there is a file in the request
-        if 'file' not in request.files:
-            return render_template('upload.html', msg='No file selected')
+
         file = request.files['file']
         manager = request.form['Man_name']
         space_no = request.form['space_name']
+        veh_ty = request.form['veh_ty']
+        
+
+        if 'file' not in request.files:
+            
+            return render_template('upload.html')
+        
         # if no file is selected
         if file.filename == '':
-            return render_template('upload.html', msg='No file selected')
+            return render_template('upload.html')
 
         if file and allowed_file(file.filename):
 
@@ -58,6 +64,8 @@ def upload_page():
 
             user_collection = mongo.db.users
 
+            vehtype_collection = mongo.db.vehtype
+
             extracted_text = ocr_core(file)
 
             pk_collection = mongo.db.lot
@@ -70,9 +78,13 @@ def upload_page():
                 user_collection.insert({'Vehicle Number' : extracted_text,'Parking Lot' : space_no,'Date' : dateStr,'In time' : timeStr,'Managed by' : manager,'Out date' : '','Out time' : ''})
                 pk_up["Space left"] = pk_up["Space left"] - 1
                 pk_collection.save(pk_up)
+                vehtype_collection.insert({'Vehicle Number' : extracted_text,'Vehicle type' : veh_ty,'Amount' : '','Payment method' : ''})
+                all_veh = mongo.db.users.find()
+                return render_template('upload.html',all_vehicles = all_veh)
 
 
             else:
+                veh = extracted_text
                 exit["Out date"] = dateStr
                 exit["Out time"] = timeStr
                 pk_al = exit["Parking Lot"]
@@ -80,17 +92,15 @@ def upload_page():
                 pk_al1 = pk_collection.find_one({'Lot Name' : pk_al})
                 pk_al1["Space left"] = pk_al1["Space left"] + 1
                 pk_collection.save(pk_al1)
+                all_veh = mongo.db.users.find()
+                return render_template('upload.html',all_vehicles = all_veh)
 
 
 
-            all_veh = mongo.db.users.find()
+            
 
             # extract the text and display it
-            return render_template('upload.html',
-                                   msg='Successfully processed',
-                                   extracted_text=extracted_text,
-                                   img_src=UPLOAD_FOLDER + file.filename,
-                                   all_vehicles = all_veh)
+            
     elif request.method == 'GET':
         all_veh = mongo.db.users.find()
         return render_template('upload.html',all_vehicles = all_veh)
@@ -127,6 +137,40 @@ def lot_page():
     pk_collection = mongo.db.lot
     pk_details = mongo.db.lot.find()
     return render_template('parking_space.html',pk_details=pk_details)
+
+
+@main.route('/uploadsu',methods=['GET', 'POST'])
+def uploadsu():
+    if request.method == 'POST':
+        amount = request.form['amount']
+        pay_m = request.form['pay_m']
+        veh = request.form['veh2']
+        vehtype_collection = mongo.db.vehtype
+        pk_up = vehtype_collection.find_one({'Vehicle Number' : veh})
+        if pk_up is None:
+            typo = mongo.db.vehtype.find()
+            return render_template('uploadsu.html',typo = typo)
+        else:
+            pk_up["Amount"] = amount
+            pk_up["Payment method"] = pay_m
+            vehtype_collection.save(pk_up)
+            typo = mongo.db.vehtype.find()
+            return render_template('uploadsu.html',typo = typo)  
+    elif request.method == 'GET':
+        typo = mongo.db.vehtype.find()
+        return render_template('uploadsu.html',typo = typo)
+    
+    
+    
+
+    
+            
+            
+            
+           
+            
+            
+
 
 
 
